99409a4201d6bdd468191ff4d868425b66131479
Giash Uddin <mdgiashu069@gmail.com>
2022-02-24 13:59:49 +0600

    leetcode

--- src/string/StringSolution2.java
+++ src/string/StringSolution2.java
@@ -1,11 +1,18 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringSolution2 {
 
     public static void main(String[] args) {
         StringSolution2 stringSolution2 = new StringSolution2();
 
-        System.out.println(stringSolution2.calculate("(1+(4+5+2)-3)+(6+8)"));
+//        System.out.println(stringSolution2.calculate("(1+(4+5+2)-3)+(6+8)"));
+
+        int[] numbers = {34323, 3432};
+        System.out.println(stringSolution2.largestNumber(numbers));
     }
 
     // Leetcode problem: 224
@@ -45,5 +52,17 @@ public class StringSolution2 {
             index++;
         }
         return sum;
+    }
+
+    // Leetcode problem: 179
+    public String largestNumber(int[] nums) {
+        List<String> stringList = new ArrayList<>(Arrays.stream(nums).mapToObj(String::valueOf).toList());
+        stringList.sort((first, second) -> {
+            String str1 = first + second;
+            String str2 = second + first;
+            return str2.compareTo(str1);
+        });
+
+        return String.join("", stringList);
     }
 }
