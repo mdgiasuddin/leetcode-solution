5f25f58644832054268cd3d5fb4f3704c653db46
Giash Uddin <mdgiashu069@gmail.com>
2022-02-27 10:48:47 +0600

    leetcode

--- src/string/StringSolution3.java
+++ src/string/StringSolution3.java
@@ -1,6 +1,7 @@
 package string;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class StringSolution3 {
     public static void main(String[] args) {
@@ -9,6 +10,11 @@ public class StringSolution3 {
     }
 
     // Leetcode problem: 556
+    /*
+     * Traverse from right side and find first digit which is greater than left digit
+     * Sort the right side
+     * Find the appropriate place for the digit which is smaller
+     * */
     public int nextGreaterElement(int n) {
         String numString = String.valueOf(n);
 
@@ -40,4 +46,41 @@ public class StringSolution3 {
 
         return numLong > Integer.MAX_VALUE ? -1 : (int) numLong;
     }
+
+    // Leetcode problem: 678
+    /*
+     * Valid parenthesis
+     * Maintain 2 stacks (1 for '(' and 1 for '*')
+     * Balance ')' using '(' if failed then '*'
+     * Balance remaining '(' with '*'
+     * */
+    public boolean checkValidString(String s) {
+        Stack<Integer> parentheses = new Stack<>();
+        Stack<Integer> stars = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                parentheses.push(i);
+            } else if (ch == '*') {
+                stars.push(i);
+            } else {
+                if (!parentheses.isEmpty()) {
+                    parentheses.pop();
+                } else if (!stars.isEmpty()) {
+                    stars.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!parentheses.isEmpty()) {
+            if (stars.isEmpty() || parentheses.pop() > stars.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
