7c3e218fb74354bdef45b591cf056548ec28e019
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-03-06 20:50:25 +0600

    leetcode

--- src/string/StringSolution5.java
+++ src/string/StringSolution5.java
@@ -124,7 +124,139 @@ public class StringSolution5 {
         return palindromeTable[0][strLen - 1];
     }
 
+    // Leetcode problem: 91
+    /*
+     * Solve by dynamic programming
+     * */
+    public int numDecodingsI(String s) {
+        /*
+        int[] dp = new int[s.length() + 1];
+        if (s.charAt(0) == '0')
+            return 0;
+
+        // base case
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            char currentChar = s.charAt(i - 1), prevChar = s.charAt(i - 2);
+
+            // If a partition can be made with current character
+            if (currentChar != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // If a partition can be made with previous character
+            if (prevChar == '1' || (prevChar == '2' && currentChar <= '6')) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[s.length()];
+        */
+
+        // Efficient Solution without saving all the state, we can store only 2 previous state
+
+        if (s.charAt(0) == '0')
+            return 0;
+
+        if (s.length() == 1)
+            return 1;
+
+        int prevSolution, prevPrevSolution, currentSolution = 0;
+        prevPrevSolution = prevSolution = 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            currentSolution = 0;
+            char currentChar = s.charAt(i - 1), prevChar = s.charAt(i - 2);
+
+            if (currentChar != '0') {
+                currentSolution += prevSolution;
+            }
+            if (prevChar == '1' || (prevChar == '2' && currentChar <= '6')) {
+                currentSolution += prevPrevSolution;
+            }
+
+            prevPrevSolution = prevSolution;
+            prevSolution = currentSolution;
+        }
+
+        return currentSolution;
+
+    }
+
     // Leetcode problem: 639
+    /*
+     * The problem is similar of problem: 91
+     * There is '*' character extra
+     * Solve by Dynamic programming
+     * */
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0')
+            return 0;
+
+        long prevSolution, prevPrevSolution, currentSolution = 0;
+
+        // Base case 0 and 1 position
+        prevPrevSolution = 1;
+        prevSolution = s.charAt(0) == '*' ? 9 : 1;
+
+        if (s.length() == 1)
+            return (int) prevSolution;
+
+        long MOD = 1_000_000_007;
+        for (int i = 2; i <= s.length(); i++) {
+            char currentChar = s.charAt(i - 1), prevChar = s.charAt(i - 2);
+            currentSolution = 0;
+
+            // Consider only current character
+            // If character is '*' then it can be any of 9 character
+            if (currentChar == '*') {
+                currentSolution += (9 * prevSolution) % MOD;
+            } else if (currentChar != '0') {
+                currentSolution += prevSolution % MOD;
+            }
+
+            // Make pair with previous character
+            if (prevChar == '*') {
+                // If 2 '*' side by side then there are 15 options. [11, 12, ..., 19, 21, 22, ..., 26]
+                if (currentChar == '*') {
+                    currentSolution += (15 * prevPrevSolution) % MOD;
+                }
+
+                // If current char is a digit less than '6' then for '*' we have 2 option [1, 2]
+                else if (currentChar <= '6') {
+                    currentSolution += (2 * prevPrevSolution) % MOD;
+                }
+
+                // If current char is greater than '6' we have only one option for '*' [1]
+                else {
+                    currentSolution += prevPrevSolution % MOD;
+                }
+            } else if (prevChar == '1') {
+                // If prev char is '1' then there are 9 options for '*' [11, 12, ..., 19]
+                if (currentChar == '*') {
+                    currentSolution += (9 * prevPrevSolution) % MOD;
+                }
+                // If current char is any digit then only one option [1Digit]
+                else {
+                    currentSolution += prevPrevSolution % MOD;
+                }
+            } else if (prevChar == '2') {
+                // If prev char is '2' then there are 6 options for '*' [21, 22, ..., 26]
+                if (currentChar == '*') {
+                    currentSolution += (6 * prevPrevSolution) % MOD;
+                } else if (currentChar <= '6') {
+                    currentSolution += prevPrevSolution % MOD;
+                }
+            }
+
+            prevPrevSolution = prevSolution;
+            prevSolution = currentSolution;
+        }
+
+        return (int) (currentSolution % MOD);
+    }
+
     // Leetcode problem: 686
     /*
      * Math.ceil(b.length() / a.length()) or Math.ceil(b.length() / a.length()) + 1
