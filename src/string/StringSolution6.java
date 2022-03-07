4e375c9862365b442b10587fdadf9ab391e118a9
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-03-07 21:17:51 +0600

    leetcode

--- src/string/StringSolution6.java
+++ src/string/StringSolution6.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringSolution6 {
 
     public static void main(String[] args) {
@@ -7,6 +10,38 @@ public class StringSolution6 {
     }
 
     // Leetcode problem: 816
+    /*
+     * Divide the string with left & right at every position and try to put '.' at every position of left and right
+     * */
+    public List<String> ambiguousCoordinates(String s) {
+        List<String> result = new ArrayList<>();
+
+        // Skip '(' & ')'
+        for (int i = 2; i < s.length() - 1; i++) {
+            for (String left : placeDecimal(s, 1, i)) {
+                for (String right : placeDecimal(s, i, s.length() - 1)) {
+                    result.add("(" + left + ", " + right + ")");
+                }
+            }
+        }
+
+        return result;
+    }
+
+    List<String> placeDecimal(String s, int left, int right) {
+        List<String> decimalPoints = new ArrayList<>();
+
+        for (int d = 1; d <= right - left; d++) {
+            String leftString = s.substring(left, left + d);
+            String rightString = s.substring(left + d, right);
+
+            // Skip leading 0 and 0 at rightmost position after '.'
+            if ((!leftString.startsWith("0") || leftString.equals("0")) && !rightString.endsWith("0"))
+                decimalPoints.add(leftString + (d < right - left ? "." : "") + rightString);
+        }
+        return decimalPoints;
+    }
+
     // Leetcode problem: 820
     // Leetcode problem: 842
 }
