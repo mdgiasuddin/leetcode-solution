10bd0f5cefbea2a180c40f50a53d4619cc6eb492
Giash Uddin <mdgiashu069@gmail.com>
2022-03-08 16:41:33 +0600

    leetcode

--- src/string/StringSolution6.java
+++ src/string/StringSolution6.java
@@ -247,4 +247,28 @@ public class StringSolution6 {
 
         return "";
     }
+
+    // Leetcode problem: 889
+    // Leetcode problem: 916
+
+    // Leetcode problem: 921
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                stack.push(ch);
+            } else {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+
+        return stack.size();
+    }
 }
