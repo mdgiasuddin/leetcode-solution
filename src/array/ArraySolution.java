9578cbacae5b5f3afcb0f762c98f05d3981c596c
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-03-11 19:38:33 +0600

    leetcode

--- src/array/ArraySolution.java
+++ src/array/ArraySolution.java
@@ -186,4 +186,50 @@ public class ArraySolution {
 
         return result;
     }
+
+    // Leetcode problem: 18
+    /*
+     * 4 Sum
+     * This extended version of 3 Sum
+     * Run an extra loop with 3 sum & similarly avoid duplicate
+     * */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 3; i++) {
+
+            // Remove duplicate in first number
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                for (int j = i + 1; j < nums.length - 2; j++) {
+
+                    // Remove duplicate in second number
+                    if (j == i + 1 || nums[j] != nums[j - 1]) {
+                        int k = j + 1, l = nums.length - 1;
+
+                        while (k < l) {
+                            int sum = nums[i] + nums[j] + nums[k] + nums[l];
+
+                            if (sum == target) {
+                                result.add(List.of(nums[i], nums[j], nums[k], nums[l]));
+
+                                // To remove the duplicate combination skip the same number
+                                while (k < l && nums[k] == nums[k + 1]) k++;
+                                while (l > k && nums[l] == nums[l - 1]) l--;
+                                k++;
+                                l--;
+                            } else if (sum < target) {
+                                k++;
+                            } else {
+                                l--;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 }
