357adb1e9a2660a3c0584280ca999ea78d529d28
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-03-14 20:52:15 +0600

    leetcode

--- src/array/ArraySolution3.java
+++ src/array/ArraySolution3.java
@@ -68,4 +68,107 @@ public class ArraySolution3 {
 
         return dp[m - 1][n - 1];
     }
+
+    // Leetcode problem
+    /*
+     * Take 2 arrays to save the rowZero and colZero
+     * Then make the position to
+     * Memory optimization can be possible
+     * We can store rowZero to first row and colZero to First Column
+     * [0][0] is overlapping, So take 2 extra variable to store the 0'th row and 0'th column
+     * */
+    public void setZeroes(int[][] matrix) {
+
+        int rowZero = 1, colZero = 1, m = matrix.length, n = matrix[0].length;
+
+        // Traverse the first row to find any 0
+        for (int col = 0; col < n; col++) {
+            if (matrix[0][col] == 0) {
+                rowZero = 0;
+                break;
+            }
+        }
+
+        // Traverse the first column to find any 0
+        for (int row = 0; row < m; row++) {
+            if (matrix[row][0] == 0) {
+                colZero = 0;
+                break;
+            }
+        }
+
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                if (matrix[row][col] == 0) {
+                    matrix[row][0] = matrix[0][col] = 0; // Make that row and col to 0
+                }
+            }
+        }
+
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                // If there is any zero in that row or colum then make it to zero
+                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+
+        // Make the first row to 0
+        if (rowZero == 0) {
+            for (int col = 0; col < n; col++) {
+                matrix[0][col] = 0;
+            }
+        }
+
+        // Make the first colum to zero
+        if (colZero == 0) {
+            for (int row = 0; row < m; row++) {
+                matrix[row][0] = 0;
+            }
+        }
+    }
+
+
+    // Leetcode problem: 74
+    /*
+     * Search in 2D Matrix
+     * First Binary search row wise to find the row in which target can be found
+     * Then Binary search in that row
+     * */
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length, n = matrix[0].length;
+        if (target < matrix[0][0] || target > matrix[m - 1][n - 1]) {
+            return false;
+        }
+
+        int fistRow = 0, lastRow = m - 1, midRow = 0;
+        while (fistRow <= lastRow) {
+            midRow = fistRow + (lastRow - fistRow) / 2;
+
+            if (matrix[midRow][0] <= target && matrix[midRow][n - 1] >= target) {
+                break;
+            } else if (target < matrix[midRow][0]) {
+                lastRow = midRow - 1;
+            } else {
+                fistRow = midRow + 1;
+            }
+        }
+
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (matrix[midRow][mid] == target) {
+                return true;
+            } else if (target < matrix[midRow][mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
 }
