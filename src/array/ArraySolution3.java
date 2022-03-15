ee996e103ff5c21f41e098594cecdd00ad663e4e
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-03-15 09:14:33 +0600

    leetcode

--- src/array/ArraySolution3.java
+++ src/array/ArraySolution3.java
@@ -243,4 +243,22 @@ public class ArraySolution3 {
 
         return one;
     }
+
+    // Leetcode problem: 162
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    // Leetcode problem: 189
 }
