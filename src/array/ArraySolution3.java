c44893f4b846b23e23a860dc82de213fd540fc63
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-03-16 09:23:39 +0600

    leetcode

--- src/array/ArraySolution3.java
+++ src/array/ArraySolution3.java
@@ -386,18 +386,16 @@ public class ArraySolution3 {
     }
 
     // Leetcode problem: 198
-    // to complete
+    /*
+     * Dynamic programming
+     * dp[i] = Max(nums[i] + dp[i-2], dp[i-1]
+     * */
     public int rob(int[] nums) {
-        if (nums.length == 1)
-            return nums[0];
-
-        if (nums.length == 2)
-            return Math.max(nums[0], nums[1]);
 
         int[] dp = new int[nums.length];
 
         dp[0] = nums[0];
-        dp[1] = nums[1];
+        dp[1] = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < nums.length; i++) {
             dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
