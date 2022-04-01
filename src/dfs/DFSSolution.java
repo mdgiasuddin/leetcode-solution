03a49600699bcbaed44d3bd2f5a5e274c7fcd47f
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-04-01 14:52:50 +0600

    leetcode

--- src/dfs/DFSSolution.java
+++ src/dfs/DFSSolution.java
@@ -33,4 +33,24 @@ public class DFSSolution {
 
         return new Pair(withNode, withoutNode);
     }
+
+    // Leetcode problem 343
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+
+            // If i is any middle number, we can use i without breaking it
+            if (i < n)
+                dp[i] = i;
+
+            // Try to get maximum by breaking
+            // Since there is j & i - j, we can loop only left side
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(dp[i], dp[j] * dp[i - j]);
+            }
+        }
+
+        return dp[n];
+    }
 }
