98165b9eea1439dd0cd59e445f0ca8a3f7603f55
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-04-26 19:45:51 +0600

    Leetcode

--- src/indefinite/FourthSolution.java
+++ src/indefinite/FourthSolution.java
@@ -321,36 +321,6 @@ public class FourthSolution {
         return result;
     }
 
-    public boolean exist(char[][] board, boolean[][] visited, int cur, int row, int col, String word) {
-        if (cur == word.length())
-            return true;
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
-                || board[row][col] != word.charAt(cur) || visited[row][col])
-            return false;
-
-        visited[row][col] = true;
-        System.out.println("row col " + board[row][col] + " " + word.charAt(cur));
-        if (exist(board, visited, cur + 1, row - 1, col, word)
-                || exist(board, visited, cur + 1, row, col - 1, word)
-                || exist(board, visited, cur + 1, row + 1, col, word)
-                || exist(board, visited, cur + 1, row, col + 1, word))
-            return true;
-        visited[row][col] = false;
-        return false;
-    }
-
-    public boolean exist(char[][] board, String word) {
-        int row = board.length, col = board[0].length;
-        boolean[][] visited = new boolean[row][col];
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++)
-                if (exist(board, visited, 0, i, j, word))
-                    return true;
-        }
-        return false;
-    }
-
     public String reverseWords(String s) {
         int start = 0, end = s.length() - 1;
         while (start < end) {
--- src/recursion/BacktrackingSolution.java
+++ src/recursion/BacktrackingSolution.java
@@ -27,4 +27,35 @@ public class BacktrackingSolution {
         if (close < open)
             generateParenthesis(result, current + ")", n, open, close + 1);
     }
+
+    // Leetcode problem: 79
+    public boolean exist(char[][] board, String word) {
+        int row = board.length, col = board[0].length;
+        boolean[][] visited = new boolean[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++)
+                if (exist(board, visited, 0, i, j, word))
+                    return true;
+        }
+        return false;
+    }
+
+    public boolean exist(char[][] board, boolean[][] visited, int cur, int row, int col, String word) {
+        if (cur == word.length())
+            return true;
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
+                || board[row][col] != word.charAt(cur) || visited[row][col])
+            return false;
+
+        visited[row][col] = true;
+        if (exist(board, visited, cur + 1, row - 1, col, word)
+                || exist(board, visited, cur + 1, row, col - 1, word)
+                || exist(board, visited, cur + 1, row + 1, col, word)
+                || exist(board, visited, cur + 1, row, col + 1, word))
+            return true;
+
+        visited[row][col] = false;
+        return false;
+    }
 }
