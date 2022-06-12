124cbaba912ceffd97a5100b306ca3555740a8ab
Md. Giash Uddin <mdgiashu069@gmail.com>
2022-06-12 21:31:58 +0600

    Leetcode

--- src/array/ArraySolution.java
+++ src/array/ArraySolution.java
@@ -17,7 +17,7 @@ public class ArraySolution {
      * Use map
      * */
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 return new int[]{map.get(target - nums[i]), i};
