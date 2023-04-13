612075c0e7710a309932fc60453831a2f3e786ce
Md. Giash Uddin <mdgiashu069@gmail.com>
2023-04-13 11:51:49 +0600

    Leetcode

--- src/math/GeometrySolution.java
+++ src/math/GeometrySolution.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.*;
+
 public class GeometrySolution {
 
     // Leetcode problem: 1232
@@ -28,5 +30,58 @@ public class GeometrySolution {
         }
 
         return true;
+    }
+
+    // Leetcode problem: 850
+    /*
+     * Rectangle Area II.
+     * Explanation: https://www.youtube.com/watch?v=TIojOkG1E4U&list=PLEvw47Ps6OBA3ysUO1XOho_6bFsqQ9NQ8&index=19
+     * */
+    public int rectangleArea(int[][] rectangles) {
+        Set<Integer> xSet = new TreeSet<>();
+        Set<Integer> ySet = new TreeSet<>();
+
+        for (int[] rectangle : rectangles) {
+            xSet.add(rectangle[0]);
+            xSet.add(rectangle[2]);
+            ySet.add(rectangle[1]);
+            ySet.add(rectangle[3]);
+        }
+
+        List<Integer> xList = new ArrayList<>();
+        List<Integer> yList = new ArrayList<>();
+        Map<Integer, Integer> xIndex = new HashMap<>();
+        Map<Integer, Integer> yIndex = new HashMap<>();
+
+        int index = 0;
+        for (int x : xSet) {
+            xList.add(x);
+            xIndex.put(x, index++);
+        }
+        index = 0;
+        for (int y : ySet) {
+            yList.add(y);
+            yIndex.put(y, index++);
+        }
+
+        boolean[][] taken = new boolean[xSet.size()][ySet.size()];
+
+        long area = 0;
+        long mod = 1000000007;
+        for (int[] rectangle : rectangles) {
+            for (int xIdx = xIndex.get(rectangle[0]); xIdx < xIndex.get(rectangle[2]); xIdx++) {
+                for (int yIdx = yIndex.get(rectangle[1]); yIdx < yIndex.get(rectangle[3]); yIdx++) {
+                    if (!taken[xIdx][yIdx]) {
+                        taken[xIdx][yIdx] = true;
+                        long width = (long) xList.get(xIdx + 1) - xList.get(xIdx);
+                        long height = (long) yList.get(yIdx + 1) - yList.get(yIdx);
+
+                        area = (area + (width * height) % mod) % mod;
+                    }
+                }
+            }
+        }
+
+        return (int) area;
     }
 }
