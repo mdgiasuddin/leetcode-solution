3fd9371c8085688aea49f78b0d0f05c42ce91223
Md. Giash Uddin <mdgiashu069@gmail.com>
2023-10-29 11:26:05 +0600

    Leetcode.

--- src/linkedlist/LinkedListSolution3.java
+++ src/linkedlist/LinkedListSolution3.java
@@ -45,4 +45,52 @@ public class LinkedListSolution3 {
 
         return res;
     }
+
+    // Leetcode problem: 430
+    /*
+     * Flatten a Multilevel Linked List.
+     * Explanation: https://www.youtube.com/watch?v=BVDbZzhJk2E
+     * */
+    public Node flatten(Node head) {
+        Node current = head;
+        while (current != null) {
+
+            // If there is no child then continue.
+            if (current.child == null) {
+                current = current.next;
+                continue;
+            }
+
+            Node tmp = current.child;
+            // There is a child. find the tail node of the child.
+            while (tmp.next != null) {
+                tmp = tmp.next;
+            }
+
+            // Join the tail with current.next;
+            tmp.next = current.next;
+            if (current.next != null) {
+                current.next.prev = tmp;
+            }
+
+            // Update the next of current to tmp;
+            current.next = current.child;
+            current.child.prev = current;
+
+            // Remove connection with child.
+            current.child = null;
+
+            // Modify current to next for the next iteration.
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    }
 }
