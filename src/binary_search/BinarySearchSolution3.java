fa3425f5c9d0b9f34e0b850e4e42d7b6610e4e6a
giash.inument <giash.inument@bkash.com>
2025-11-03 13:38:37 +0600

    Leetcode

---
+++ src/binary_search/BinarySearchSolution3.java
@@ -0,0 +1,81 @@
+package binary_search;
+
+public class BinarySearchSolution3 {
+
+    // Leetcode problem: 2594
+
+    /**
+     * Minimum Time to Repair Cars.
+     * Explanation: https://www.youtube.com/watch?v=RHct_Pz9EBo
+     *
+     */
+    public long repairCars(int[] ranks, int cars) {
+        long l = 1;
+        long r = (long) ranks[0] * cars * cars;
+
+        long res = -1;
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+            if (repairedCars(ranks, m) >= cars) {
+                res = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return res;
+    }
+
+    private long repairedCars(int[] ranks, long time) {
+        long count = 0;
+        for (int rank : ranks) {
+            count += (long) Math.sqrt((double) time / rank);
+        }
+
+        return count;
+    }
+
+
+    // Leetcode problem: 2560
+
+    /**
+     * House Robber IV.
+     * Explanation: https://www.youtube.com/watch?v=OHZqAc6h3Us
+     */
+    public int minCapability(int[] nums, int k) {
+        int l = 1;
+        int r = 0;
+        for (int num : nums) {
+            r = Math.max(r, num);
+        }
+
+        int res = 0;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (isCapable(nums, m, k)) {
+                res = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return res;
+    }
+
+    private boolean isCapable(int[] nums, int value, int k) {
+        int i = 0;
+        int count = 0;
+        while (i < nums.length) {
+            if (nums[i] <= value) {
+                count += 1;
+                i += 2;
+            } else {
+                i += 1;
+            }
+        }
+
+        return count >= k;
+    }
+}
